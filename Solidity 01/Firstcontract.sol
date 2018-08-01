2b50bfc62c30bc9ab7581130814a316b9b5ccd0a
YazanKhatib <yazankhatib97@gmail.com>
2018-08-01 15:24:34 +0300

    Solidity tutorial 01

---
+++ Solidity 01/Firstcontract.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.23; 
+
+contract Firstcontract{
+    string private name ;
+    uint private age;
+    
+    function setName(string _name) public{
+        name = _name; 
+    }
+    
+    function getName() view public returns(string) {
+        return name; 
+    }
+    
+    function setAge(uint _age) public {
+        age = _age;
+    }
+    
+    function getAge() view public returns(uint) {
+        return age; 
+    }
+}
