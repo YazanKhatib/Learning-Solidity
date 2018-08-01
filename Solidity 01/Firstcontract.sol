3e2e5c7bf3149aaa73c5ca4f03124cfa619bfffb
YazanKhatib <yazankhatib97@gmail.com>
2018-08-01 15:54:46 +0300

    Solidity tutorial 01 enhancement

--- Solidity 01/Firstcontract.sol
+++ Solidity 01/Firstcontract.sol
@@ -1,22 +1,21 @@
-pragma solidity ^0.4.23; 
-
+pragma solidity ^0.4.23;
 contract Firstcontract{
     string private name ;
     uint private age;
-    
+
     function setName(string _name) public{
-        name = _name; 
+        name = _name;
     }
-    
+
     function getName() view public returns(string) {
-        return name; 
+        return name;
     }
-    
+
     function setAge(uint _age) public {
         age = _age;
     }
-    
+
     function getAge() view public returns(uint) {
-        return age; 
+        return age;
     }
 }
